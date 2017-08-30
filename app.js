818f2195441019135d188db78147ddc47c24d60f
Jennifer Elton <justjenny40@hotmail.com>
2017-08-29 20:23:29 -0700

    contains beginning of constructor function, I need help with constructor functions

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// contsructor function for portfolio
+
+function projects ( title, image, URL); {
+this.title = title;
+this.image = image;
+this.Url = Url;
+
+this.render();
+
+}
+
+projects.prototype.render = function () {
+    var 
+}
+
